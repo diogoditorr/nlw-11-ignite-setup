fe06a31efe44f769bdfcc9f7c7c85c4a5a5dfe8e
diogoditorr <35296262+diogoditorr@users.noreply.github.com>
2023-01-21 23:55:52 -0300

    feat: setup server project

---
+++ server/src/server.ts
@@ -0,0 +1,24 @@
+import Fastify from "fastify";
+import cors from "@fastify/cors";
+import { PrismaClient } from "@prisma/client";
+
+const app = Fastify();
+const prisma = new PrismaClient();
+
+app.register(cors);
+
+app.get("/", async () => {
+    const habits = await prisma.habit.findMany({
+        where: {
+            title: {
+                startsWith: "Beber",
+            },
+        },
+    });
+
+    return habits;
+});
+
+app.listen({ port: 3333 }).then((url) => {
+    console.log("[HTTP] Server running at:", url);
+});
